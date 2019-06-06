929f59698bd3de892ef9568b046ec9821e4b5227
tlacombe <hoetre@hotmail.fr>
2019-06-06 17:06:00 +0200

    update test file and tutorial now that the bug is solved

--- test.py
+++ test.py
@@ -18,6 +18,8 @@ layer_type = "im"
 perm_op = "sum"
 keep = 5  # only useful if perm_op = "topk"
 
+weight="grid"
+grid_size=[10, 10]
 # Parameter specific to layer_type="im"
 image_size=[10, 10]
 # Parameter specific to layer_type="gs"
@@ -29,6 +31,7 @@ num_samples = 50
 
 perslayParameters = {"layer_type":layer_type,
                      "perm_op": perm_op, "keep":keep,
+                    "weight":weight, "grid_size":grid_size,
                     "image_size": image_size,
                     "num_gaussians": num_gaussians,
                     "pm_dimension": d,
@@ -36,4 +39,9 @@ perslayParameters = {"layer_type":layer_type,
 
 model = baseModel(perslayParameters, filts, labels)
 
-single_run(dataset, model)
+decay = 0.9
+lr = 0.01
+num_epoch = 100
+optim_parameters = {"decay":decay,"lr":lr, "num_epoch":num_epoch}
+
+single_run(diags, feats, labels, filts, model, optim_parameters)
