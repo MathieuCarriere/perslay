02b31da4e6292464bef1f3537bdc25fbbc9e06f0
martinroyer <martinpierreroyer@gmail.com>
2019-09-10 11:17:08 +0200

    some versioning

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author_email                   = "mathieu.carriere3@gmail.com",
     description                    = "A tensorflow layer for handling persistence diagrams in neural networks",
     packages                       = ["perslay"],
+    version                        = '1.0.1',
     long_description_content_type  = "text/markdown",
     long_description               = open("README.md", "r").read(),
     url                            = "https://github.com/MathieuCarriere/perslay/",
